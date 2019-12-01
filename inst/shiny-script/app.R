e11cf7201e02197ac5b47b4c32584bc2b1347518
Yuexin Yu <yuexin.yu@mail.utoronto.ca>
2019-11-30 22:57:56 -0500

    ui 0.0.1 update

--- inst/shiny-script/app.R
+++ inst/shiny-script/app.R
@@ -1,10 +1,18 @@
 library(shiny)
 
 ui <- fluidPage(
-  sliderInput(inputId = "num",
-              label = "Choose a number",
-              value = 25, min = 1, max = 100),
-  plotOutput("hist")
+  shinythemes::themeSelector(),
+  navbarPage(
+    # theme = "cerulean",  # <--- To use a theme, uncomment this
+    "shinythemes",
+    tabPanel("3D alignment of homology proteins",
+             sidebarPanel(
+               selectInput('alignedPath', 'Aligned file', names(a3v)),
+               textInput('targetName', 'Target name', "general"),
+               textInput('templateName', 'Template name', "general")
+               )
+    )
+  )
 )
 
 server <- function(input, output){
