7a9c4e97daec76970160ae5b79f4591dddaca3f4
Yuexin Yu <yuexin.yu@mail.utoronto.ca>
2019-12-04 16:47:58 -0500

    shiny 3dalignemnt function developed

--- inst/shiny-script/app.R
+++ inst/shiny-script/app.R
@@ -1,24 +1,48 @@
 library(shiny)
+library(rgl)
 
-ui <- fluidPage(
-  shinythemes::themeSelector(),
+ui <- tagList(
   navbarPage(
-    # theme = "cerulean",  # <--- To use a theme, uncomment this
-    "shinythemes",
+    "homolofy modelling template preparation (hmtp)",
     tabPanel("3D alignment of homology proteins",
              sidebarPanel(
-               selectInput('alignedPath', 'Aligned file', names(a3v)),
-               textInput('targetName', 'Target name', "general"),
-               textInput('templateName', 'Template name', "general")
-               )
+               helpText("The aligned pdb file can be generated from http://fatcat.burnham.org/fatcat-cgi/cgi/fatcat.pl?-func=pairwise."),
+
+               fileInput("alignedFile", label = h3("Aligned file")),
+               helpText("The aligned file must be in .pdb"),
+
+               textInput('targetName', 'Target name'),
+               helpText("The target will be red in the visualization."),
+
+               textInput('templateName', 'Template name'),
+               helpText("The template will be blue in the visualization."),
+
+               submitButton("Submit")
+               ),
+             mainPanel(
+               rglwidgetOutput(outputId = "plot"),
+               verbatimTextOutput("text")
+             )
     )
+
+
+
   )
+
 )
 
 server <- function(input, output){
-  output$hist <- renderPlot({
-    hist(rnorm(input$num))
+  output$plot <- renderRglwidget({
+    # Generate by functions
+    hmtp::alignment3dVisualization(input$alignedFile$datapath,
+                                   input$targetName,
+                                   input$templateName)
   })
+  output$text <- renderText({
+    paste("Red is", input$targetName, "\nBlue is", input$templateName)
+  })
+
+
 }
 
 shinyApp(ui = ui, server = server)
